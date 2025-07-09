b1f8a7c63607381333df64ef08110820189da0de
garv34 <153420226+garv34@users.noreply.github.com>
2025-07-10 00:41:47 +0530

    Create slidebar.jsx

---
+++ slidebar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h2>Ping<span>Notes</span></h2>
+      <ul>
+        <li>Dashboard</li>
+        <li>Notes</li>
+        <li>Users</li>
+        <li>Teachers</li>
+        <li>Groups</li>
+        <li>Subjects</li>
+        <li>Files</li>
+        <li>Admin</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
